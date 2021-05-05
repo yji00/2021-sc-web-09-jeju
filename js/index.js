73028bca6beac968ddf8715e3c37118132513943
yji00 <dksro333@naver.com>
2021-05-06 00:51:47 +0900

    slick변경

--- js/index.js
+++ js/index.js
@@ -13,6 +13,32 @@ else idx = idx + 1 */
 /*************** Index *****************/
 $(function () {
 
+	var slick = {
+		autoplay: true,
+		autoplaySpeed: 2000,
+		infinite: true,
+		touchThreshold: 10,
+		arrows: false,
+		dots: true,
+		speed: 500,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	}
+
 	weather();
 	setCookie();
 	slideMain();
@@ -169,20 +195,27 @@ $(function () {
 	}
 
 	function slideDream() {
-		var swiper = getSwiper('.dream-wrapper', {
-			break: 3
-		});
+		// var swiper = getSwiper('.dream-wrapper', { break: 3 });
+		var $slick = $('.dream-wrapper .slide-wrapper');
+		var $btPrev = $('.dream-wrapper .bt-slide.left');
+		var $btNext = $('.dream-wrapper .bt-slide.right');
+		var options = cloneObject(slick);
+		$slick.slick(options);
+		makeSlickButton($slick, $btPrev, $btNext);
 	}
 
 	function slidePromo() {
-		var $promoWrapper = $('.promo-wrapper');
-		var $slideWrapper = $promoWrapper.find('.slide-wrapper');
+		var $promo = $('.promo-wrapper');
+		var $slick = $promo.find('.slide-wrapper');
+		var $btPrev = $promo.find('.bt-slide.left');
+		var $btNext = $promo.find('.bt-slide.right');
+		var options = cloneObject(slick);
 
 		function onGetData(r) {
 			// for(var i=0; i<r.promo.length; i++) {}
 			r.promo.forEach(function (v, i) {
 				var html = '';
-				html += '<li class="slide swiper-slide">';
+				html += '<li class="slide">';
 				html += '<div class="img-wrap ratio-wrap" data-ratio="1">';
 				html += '<div class="ratio-bg" style="background-image: url(' + v.src + ');"></div>';
 				html += '</div>';
@@ -191,46 +224,49 @@ $(function () {
 				html += '<div class="desc">' + v.desc + '</div>';
 				html += '</div>';
 				html += '</li>';
-				$slideWrapper.append(html);
-			})
-			var swiper = getSwiper('.promo-wrapper', {
-				break: 4,
-				pager: false
+				$slick.append(html);
 			});
+			
+			options.slidesToShow = 4;
+			options.dots = false;
+			options.responsive.unshift({breakpoint: 992, settings: {slidesToShow: 3}});
+			$slick.slick(options);
+			makeSlickButton($slick, $btPrev, $btNext);
+			$(window).trigger('resize');
 		}
 		$.get('../json/promotion.json', onGetData); // init
 	}
 
 	function initStyle() {
-		$(window).resize(onResize).trigger('resize');
-
-		function onResize() {
-			$('.style-wrapper .ratio-wrap').each(function (i) {
-				var ratio = $(this).data('ratio');
-				var width = $(this).innerWidth();
-				var height = width * Number(ratio);
-				$(this).innerHeight(height);
-			})
-		}
+		
 	}
 
 	function slideRoom() {
-		var room = [],
-			swiper;
+		var room = [], swiper;
+		var $room = $('.room-wrapper');
+		var $slick = $room.find('.slide-wrapper');
 		var $movingBox = $('.room-wrapper .desc-wrapper .moving-box');
 		var $tag = $('.room-wrapper .desc-wrapper .tag > div');
 		var $title = $('.room-wrapper .desc-wrapper .title > div');
 		var $desc = $('.room-wrapper .desc-wrapper .desc > div');
+		var $btPrev = $room.find('.bt-slide.left');
+		var $btNext = $room.find('.bt-slide.right');
+		var options = cloneObject(slick);
 
 		function onGetData(r) {
 			room = r.room.slice();
-			console.log(room);
-			swiper = getSwiper('.room-wrapper', {
-				break: 2,
-				speed: 600
-			});
-			swiper.on('slideChange', onBefore);
-			swiper.on('slideChangeTransitionEnd', onAfter);
+			options.autoplaySpeed = 4000;
+			options.slidesToShow = 2;
+			options.dots = false;
+			options.responsive.pop();	// 배열의 마지막 요소를 빼낸다
+			options.responsive[0].breakpoint = 992;
+			options.responsive[0].settings.slidesToShow = 1;
+			$slick.slick(options);
+			makeSlickButton($slick, $btPrev, $btNext);
+			$(window).trigger('resize');
+
+			$slick.on('beforeChange', onBefore);
+			$slick.on('afterChange', onAfter);
 			showDesc(0);
 		}
 
@@ -238,8 +274,7 @@ $(function () {
 			$movingBox.removeClass('active');
 		}
 
-		function onAfter() {
-			var idx = this.realIndex;
+		function onAfter(e, slick, idx) {
 			showDesc(idx);
 		}
 
@@ -253,59 +288,74 @@ $(function () {
 	}
 
 	function slideSvc() {
-		var $slideWrapper = $('.svc-wrapper .slide-wrapper');
-		var swiper, lastIdx;
+		var $svc = $('.svc-wrapper');
+		var $slick = $svc.find('.slide-wrapper');
+		var $btPrev = $svc.find('.bt-slide.left');
+		var $btNext = $svc.find('.bt-slide.right');
+		var options = cloneObject(slick);
+		var lastIdx;
 
 		function onGetData(r) {
 			lastIdx = r.svc.length - 1;
 			r.svc.forEach(function (v, i) {
 				var html = '';
-				html += '<li class="slide swiper-slide" title="' + i + '">';
+				html += '<li class="slide" title="' + i + '">';
 				html += '<div class="img-wrap">';
 				html += '<img src="' + v.src + '" alt="svc" class="w-100">';
 				html += '</div>';
 				html += '<h4 class="title">' + v.title + '</h4>';
 				html += '</li>';
-				$slideWrapper.append(html);
+				$slick.append(html);
 			})
-			swiper = getSwiper('.svc-wrapper', {
-				break: 2,
-				speed: 600,
-				pager: false
-			});
-			swiper.on('slideChange', onChange);
+			options.slidesToShow = 2;
+			options.dots = false;
+			options.responsive.pop();
+			options.responsive[0].breakpoint = 992;
+			options.responsive[0].settings.slidesToShow = 1;
+			$slick.slick(options);
+			makeSlickButton($slick, $btPrev, $btNext);
+			$slick.on('beforeChange', onBefore);
+			$(window).trigger('resize');
 			showAni(1);
 		}
 
-		function onChange() {
-			showAni((this.realIndex == lastIdx) ? 0 : this.realIndex + 1);
+		function onBefore(e, slick, current, idx) {
+			showAni((idx === lastIdx) ? 0 : idx + 1);
 		}
 
 		function showAni(n) {
-			$slideWrapper.find('.slide').removeClass('active');
-			$slideWrapper.find('.slide[title="' + n + '"]').addClass('active');
+			$slick.find('.slide').removeClass('active');
+			$slick.find('.slide[title="' + n + '"]').addClass('active');
 		}
 		$.get('../json/svc.json', onGetData);
 	}
 
 	function slideSns() {
-		var $slideWrapper = $('.sns-wrapper .slide-wrapper');
-		var swiper;
+		var $sns = $('.sns-wrapper');
+		var $slick = $sns.find('.slide-wrapper');
+		var $btPrev = $sns.find('.bt-slide.left');
+		var $btNext = $sns.find('.bt-slide.right');
+		var options = cloneObject(slick);
 
 		function onGetData(r) {
 			r.sns.forEach(function (v, i) {
 				var html = '';
-				html += '<li class="slide swiper-slide">';
+				html += '<li class="slide">';
 				html += '<img src="' + v.src + '" alt="이벤트" class="w-100">';
 				html += '<i class="icon fab fa-instagram"></i>';
 				html += '</li>';
-				$slideWrapper.append(html);
+				$slick.append(html);
 			})
-			swiper = getSwiper('.sns-wrapper', {
-				break: 7,
-				space: 0,
-				pager: false
-			});
+			options.dots = false;
+			options.slidesToShow = 7;
+			options.responsive[0].settings.slidesToShow = 3;
+			options.responsive[1].settings.slidesToShow = 2;
+			options.responsive.unshift({breakpoint: 992, settings: {slidesToShow: 4}});
+			options.responsive.unshift({breakpoint: 1200, settings: {slidesToShow: 5}});
+			options.responsive.unshift({breakpoint: 1400, settings: {slidesToShow: 6}});
+			console.log(options.responsive)
+			$slick.slick(options);
+			makeSlickButton($slick, $btPrev, $btNext);
 		}
 		$.get('../json/sns.json', onGetData);
 	}
@@ -344,7 +394,7 @@ $(function () {
 		}
 
 		function onSubmit(e) {
-			e.preventDefault();	// submit이므로 전송되어야 하는데 전송기능을 막는다.
+			e.preventDefault(); // submit이므로 전송되어야 하는데 전송기능을 막는다.
 			$form[0].contact_number.value = Math.random() * 100000 | 0;
 			emailjs.sendForm('service_gmail', 'template_gmail', this).then(function () {
 				alert('뉴스레터 신청이 완료되었습니다.');
@@ -359,6 +409,35 @@ $(function () {
 		}
 
 		/********* User Function *********/
-		emailjs.init('user_8cQZLxtTD2G7TW9Ak1fik');	// 본인거로 꼭 바꿔넣으세요.
+		emailjs.init('user_8cQZLxtTD2G7TW9Ak1fik'); // 본인거로 꼭 바꿔넣으세요.
 	}
+	
+	
+	/********* Global Function *********/
+	function makeSlickButton($slick, $prev, $next) {
+		$prev.click(function() { 
+			$slick.slick('slickPrev') 
+		});
+		$next.click(function() { 
+			$slick.slick('slickNext') 
+		});
+		$slick.find('.slick-dots').on('mouseenter', function() {
+			$slick.slick('slickPause');
+		});
+		$slick.find('.slick-dots').on('mouseleave', function() {
+			$slick.slick('slickPlay');
+		});
+	}
+	
+	function onResize(e) {
+		$('.ratio-wrap').each(function(i) {
+			var ratio = $(this).data('ratio');
+			var width = $(this).innerWidth();
+			var height = width * Number(ratio);
+			$(this).innerHeight(height);
+		})
+	}
+	
+	
+	$(window).resize(onResize).trigger('resize');
 })
